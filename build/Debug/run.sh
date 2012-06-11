ad26768e9cdb9e0c4dab81bb52769352434fc86d
David Higgins <dave@uiginn.com>
2012-06-11 14:26:25 +0200

    Revert "test of git branching"
    This reverts commit 07d224ab74228056ea70638125d4359d8ff7f3dc.

--- build/Debug/run.sh
+++ build/Debug/run.sh
@@ -1,5 +1,8 @@
+<<<<<<< HEAD
 ./PurkinjePlasticity parameters.txt log.txt purkinje_1_%.4d.dat
 <<<<<<< HEAD
 =======
 >>>>>>> 07d224a... test of git branching
+=======
+>>>>>>> f58c712... Revert "test of git branching"
 
