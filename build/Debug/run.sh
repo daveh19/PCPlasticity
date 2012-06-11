aeb00241bdc7bd9add73142fb13d219d86195448
David Higgins <dave@uiginn.com>
2012-06-11 14:44:56 +0200

    Git: after rebase test

--- build/Debug/run.sh
+++ build/Debug/run.sh
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 ./PurkinjePlasticity parameters.txt log.txt purkinje_1_%.4d.dat
-<<<<<<< HEAD
-=======
->>>>>>> 07d224a... test of git branching
-=======
->>>>>>> f58c712... Revert "test of git branching"
 
