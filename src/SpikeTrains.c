359f1d14d469da3f8e81a625df746d14b0827aa9
David Higgins <dave@uiginn.com>
2013-07-04 14:46:17 +0200

    Version used for presentation at ENS.

--- src/SpikeTrains.c
+++ src/SpikeTrains.c
@@ -951,7 +951,7 @@ int train30(unsigned int * preT, unsigned int * postT, unsigned int simulation_d
 	// Safo LTD values
 	inter_pf_gap = 10;
 	no_pf_stims = 7;
-	cf_offset = 80;
+	cf_offset = 500;
 	cf_offset += 30; // 30 accounts for middle (4th) spike
 	
     for (i = 0; i < recurrent_no_stims; i++){
@@ -1064,6 +1064,10 @@ int train33(unsigned int * preT, unsigned int * postT, unsigned int simulation_d
 	inter_pf_gap = 5; // ms
 	no_pf_stims = 5;
 	cf_offset = 40;
+	
+	// try new parameters
+	no_pf_stims = 2;
+	cf_offset = 0;
 	
     for (i = 0; i < recurrent_no_stims; i++){
 		for (int j = 0; j < no_pf_stims; j++){
