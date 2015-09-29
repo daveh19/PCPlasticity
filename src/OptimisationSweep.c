f69f1b1727f93572dcece6436c14d27dbba4bb42
David Higgins (TUPro) <dave@uiginn.com>
2015-09-29 17:08:10 +0200

    Large step size for Dc and Dn.

--- src/OptimisationSweep.c
+++ src/OptimisationSweep.c
@@ -85,17 +85,17 @@ int main( int argc, char *argv[] ){
     gsl_vector_set(x, 11, 0); // Vmax
 	// step sizes (different per parameter)
 	step_size = gsl_vector_alloc(p);
-	 gsl_vector_set(step_size, 0, 1); // tau_c
-	 gsl_vector_set(step_size, 1, 1); // Delay C
-	 gsl_vector_set(step_size, 2, 1e-2);
-	 gsl_vector_set(step_size, 3, 1e-2);
-	 gsl_vector_set(step_size, 4, 1e-2);
-	 gsl_vector_set(step_size, 5, 1e-2);
-	 gsl_vector_set(step_size, 6, 1e-6);
-	 gsl_vector_set(step_size, 7, 1e-6);
-	 gsl_vector_set(step_size, 8, 1); // tau_no, c_depol, tau_v
+	gsl_vector_set(step_size, 0, 1); // tau_c
+	gsl_vector_set(step_size, 1, 1e2); // Delay C
+	gsl_vector_set(step_size, 2, 1e-2);
+	gsl_vector_set(step_size, 3, 1e-2);
+	gsl_vector_set(step_size, 4, 1e-2);
+	gsl_vector_set(step_size, 5, 1e-2);
+	gsl_vector_set(step_size, 6, 1e-6);
+	gsl_vector_set(step_size, 7, 1e-6);
+	gsl_vector_set(step_size, 8, 1); // tau_no, c_depol, tau_v
     gsl_vector_set(step_size, 9, 1e-2); // Vjump
-    gsl_vector_set(step_size, 10, 1); // Delay NO
+    gsl_vector_set(step_size, 10, 1e2); // Delay NO
     gsl_vector_set(step_size, 11, 1e-2); // Vmax
 	// tau, D, C_pf, C_cs, N_pf, theta_d, gamma_d, gamma_p
 	//double x_init[8] = {185,(80./dt),0.07,0.6,0.2,0.522,2.3809e-4,7.9365e-5};
